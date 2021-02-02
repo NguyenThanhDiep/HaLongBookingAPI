c11b3ec6421549138a2dd61b4101c1995e8c3469
Diep-NGUYEN <thanh-diep.nguyen@bollore.com>
2021-02-02 17:59:56 +0700

    Send email for admin

--- HaLongBookingAPI/app/views.py
+++ HaLongBookingAPI/app/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 from django.db.models import Count
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Manage Booking Views
 class HotelView(APIView):
@@ -110,6 +113,51 @@ class BookingView(APIView):
         if status is not None: 
             bookings = bookings.filter(status=status)
         serializer = BookingSerializer(bookings, many=True)
+        
+        #Send Email to Admin
+        sender_email = "diep.nguyen21051995@gmail.com"
+        receiver_email = "thanh-diep.nguyen@bollore.com"
+        password = "realmadrid1995"
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "[Auto Email] New Booking"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        # Create the plain-text and HTML version of your message
+        #text = """\
+        #Hi,
+        #How are you?
+        #Real Python has many great tutorials:
+        #www.realpython.com"""
+        html = """\
+        <html>
+          <body>
+            <p>A new booking has just created.<br>
+               Please go to admin page for more details<br>
+               <i>Have a good day!<i>
+            </p>
+          </body>
+        </html>
+        """
+
+        # Turn these into plain/html MIMEText objects
+        # part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        # message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(
+                sender_email, receiver_email, message.as_string()
+            )
+
         return Response(serializer.data)
 
     def post(self, request, format=None):
